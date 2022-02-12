f528986057862489763facfc85b21edb3a5b1786
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-12 20:38:26 +0330

    start next panel project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const path = require("path");
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+
+// // next.config.js
+// const withSass = require("@zeit/next-sass");
+// module.exports = withSass();
+
+
---
+++ src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "../../components/About";
+
+function index() {
+  return (
+    <div>
+      <About />
+    </div>
+  );
+}
+
+export default index;
