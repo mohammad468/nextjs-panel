5d55386a8d57a020dad87c6d5c12b4aaea2f0c25
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-13 13:09:52 +0330

    create cols

---
+++ src/components/left-nav/LeftNav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LeftNav() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+}
+
+export default LeftNav;
---
+++ src/components/main/MainHome.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainHome() {
+  return (
+    <div>
+      <h1>MainHome</h1>
+    </div>
+  );
+}
+
+export default MainHome;
