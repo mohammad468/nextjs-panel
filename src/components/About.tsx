9950361153f8e14f49748a273f6234fb8f7c9412
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-13 12:23:05 +0330

    install redux and extentions

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1 className="about-header">Hello about</h1>
+      <h1 className="about-header">Hello admin panel</h1>
     </div>
   );
 }
